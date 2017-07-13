74117e7cbc7ba32c86ac610082f23f04a0fe0bc4
thisisaaronland <whosonfirst@mapzen.com>
2017-07-13 22:20:51 +0000

    stub out IncludeFoo properties

--- transformations/github.commits.go
+++ transformations/github.commits.go
@@ -10,13 +10,23 @@ import (
 	_ "log"
 )
 
+// see also: https://github.com/whosonfirst/go-whosonfirst-updated/issues/8
+
 type GitHubCommitsTransformation struct {
 	webhookd.WebhookTransformation
+	IncludeAdditions     bool
+	IncludeModifications bool
+	IncludeDeletions     bool
 }
 
 func NewGitHubCommitsTransformation() (*GitHubCommitsTransformation, error) {
 
-	p := GitHubCommitsTransformation{}
+	p := GitHubCommitsTransformation{
+		IncludeAdditions:     true,
+		IncludeModifications: true,
+		IncludeDeletions:     true,
+	}
+
 	return &p, nil
 }
 
@@ -40,19 +50,25 @@ func (p *GitHubCommitsTransformation) Transform(body []byte) ([]byte, *webhookd.
 
 	for _, c := range event.Commits {
 
-		for _, path := range c.Added {
-			commit := []string{commit_hash, repo_name, path}
-			wr.Write(commit)
+		if p.IncludeAdditions {
+			for _, path := range c.Added {
+				commit := []string{commit_hash, repo_name, path}
+				wr.Write(commit)
+			}
 		}
 
-		for _, path := range c.Modified {
-			commit := []string{commit_hash, repo_name, path}
-			wr.Write(commit)
+		if p.IncludeModifications {
+			for _, path := range c.Modified {
+				commit := []string{commit_hash, repo_name, path}
+				wr.Write(commit)
+			}
 		}
 
-		for _, path := range c.Removed {
-			commit := []string{commit_hash, repo_name, path}
-			wr.Write(commit)
+		if p.IncludeDeletions {
+			for _, path := range c.Removed {
+				commit := []string{commit_hash, repo_name, path}
+				wr.Write(commit)
+			}
 		}
 	}
 
