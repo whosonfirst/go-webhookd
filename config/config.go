da9489e1194033607c7c05190986327d8ed7cf6c
thisisaaronland <devnull@localhost>
2019-02-13 13:06:55 -0800

    wrangle config stuff

--- config/config.go
+++ config/config.go
@@ -33,6 +33,8 @@ type WebhookDispatcherConfig struct {
 	Port    int    `json:"port,omitempty"`
 	Channel string `json:"channel,omitempty"`
 	Config  string `json:"config,omitempty"`
+	DSN string `json:"dsn,omitempty"`
+	Function string `json:"function,omitempty"`
 }
 
 type WebhookTransformationConfig struct {
